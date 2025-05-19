20f9db048899ee16cbe44b3fddef0de64c24f232
WenxingX-GH <wenxingx@gmail.com>
2025-05-19 22:14:48 +0200

    reset the data_cleaning and raw data, add renaming part in the reprot rmd

--- code/1_Data cleaning.R
+++ code/1_Data cleaning.R
@@ -23,7 +23,28 @@ dim(df.food_time)
 head(df.food_time)
 summary(df.food_time)
 
+# **************************** PART 2: Renaming column names to snake case (incl. more descriptive names)
 
+df.food_time <- df.food_time %>%
+  rename(
+    order_id = ID,
+    courier_id = Delivery_person_ID,
+    courier_age_years = Delivery_person_Age,
+    courier_rating_1_to_5 = Delivery_person_Ratings,
+    restaurant_latitude_deg = Restaurant_latitude,
+    restaurant_longitude_deg = Restaurant_longitude,
+    customer_latitude_deg = Delivery_location_latitude,
+    customer_longitude_deg = Delivery_location_longitude,
+    order_type = Type_of_order,
+    vehicle_type = Type_of_vehicle,
+    temperature_celsius = temperature,
+    humidity_percent = humidity,
+    precipitation_mm = precipitation,
+    weather_description = weather_description,
+    traffic_level = Traffic_Level,
+    distance_km = Distance..km.,
+    delivery_time_min = TARGET
+  )
 # **************************** PART 3: Cleaning the data
 
 
